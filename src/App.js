1319668e537179d29da3addaa20976493a75c687
Jos O'shea <jos.oshea.ky@gmail.com>
2021-10-31 17:46:03 -0400

    Added CrabGoddess Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import PlayerCard from "./PlayerCard.js";
-
+import CrabGoddess from "./CrabGoddess.js";
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <CrabGoddess />
         <PlayerCard />
       </div>
     );
